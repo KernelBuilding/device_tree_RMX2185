5e09239f72ed9bf3aebb4cb892a4ac0e4b8f3e10
ProjectKernel <155278452+KernelBuilding@users.noreply.github.com>
2024-03-15 00:33:27 +0700

    Remove Unused File & Fixing Error Build a13 - a14

--- device.mk
+++ device.mk
@@ -16,9 +16,6 @@
 
 DEVICE_PATH := device/realme/RMX2185
 
-# Installs gsi keys into ramdisk, to boot a GSI with verified boot.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
-
 # Call proprietary blob setup
 $(call inherit-product, vendor/realme/RMX2185/RMX2185-vendor.mk)
 
@@ -40,7 +37,6 @@ TARGET_SCREEN_WIDTH := 720
 
 # Audio
 PRODUCT_PACKAGES += \
-    audio.a2dp.default \
     GoogleCameraGo
 
 PRODUCT_COPY_FILES += \
